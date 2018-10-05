9cff2afc943ded6a09f115a71443d9956727b5be
max.chen <amingccc@gmail.com>
2018-10-05 18:58:32 +0800

    搭建工程，商品列表后端代码

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌接口
  * @author Administrator
@@ -12,5 +14,13 @@ import com.pinyougou.pojo.TbBrand;
 public interface BrandService {
 
 	public List<TbBrand> findAll();
+	/**
+	 * 品牌分页
+	 * @param pageNum 当前页面
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
 	
+
 }
