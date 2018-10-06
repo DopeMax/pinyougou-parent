3282401f2aca5e7d62c5d9ca0b44f74e49e65311
max.chen <amingccc@gmail.com>
2018-10-06 20:14:54 +0800

    品牌管理的，添加，修改，删除，查询

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -21,6 +21,37 @@ public interface BrandService {
 	 * @return
 	 */
 	public PageResult findPage(int pageNum,int pageSize);
+	/**
+	 * 增加
+	*/
+	public void add(TbBrand brand);
 	
+	/**
+	 * 根据ID查询实体
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
+	
+
+	/**
+	 * 修改
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long [] ids);
+	
+	/**
+	 * 分页
+	 * @param pageNum 当前页 码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand, int pageNum,int pageSize);
 
 }
